fa6fedd6cffd4b219bcd1c07f9aa3b9999cca01d
sathiyavishwanath <sathiyavish123@yahoo.com>
2016-02-01 18:51:12 +0530

    Create nextprime.c

---
+++ nextprime.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+int num=8,i,j,flag; 	
+printf("enter the number");
+scanf("%d",&num);
+if(num<2)
+{
+    return 2;
+}
+for(i=num; i>0; i++)
+{
+    for(j=2; j<i; j++)
+    {
+        if((i%j)!=0)
+        {
+            flag=1;
+        }
+        else
+        {
+            flag=0;
+            break;
+        }
+    }
+    if(flag==1)
+    {
+        printf("%d",i);
+        break;
+    }
+}
+return 0;
+}
