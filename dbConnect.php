c5392adbc68a4864cc414b1db9707009ff56df3a
Sathiyapriya <sathiyavish@gmail.com>
2016-02-20 19:27:46 +0530

    Create dbConnect.php

---
+++ dbConnect.php
@@ -0,0 +1,8 @@
+<?php
+ define('HOST','mysql.hostinger.in');
+define('USER','u283046116_efghi');
+define('PASS','sakthi1995');
+define('DB','u283046116_abcde');
+ 
+ //Connecting to database
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
